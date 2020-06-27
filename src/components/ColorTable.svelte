d0f1e719748cd2a96adf9f65a349423ec58ca42e
Eric Rovell <ericrovell@gmail.com>
2020-06-27 23:45:11 +0300

    table for representing color props

---
+++ src/components/ColorTable.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let value;
+</script>
+
+<style>
+  .grid-view {
+    display: grid;
+    grid-auto-flow: column;
+    column-gap: 1em;
+  }
+
+  .grid-view > div {
+    display: grid;
+    place-items: center;
+    row-gap: 1em;
+  }
+</style>
+
+<div class="grid-view">
+  {#each Object.entries(value) as [ prop, val ]}
+    <div>
+      <span>{prop}</span>
+      <span>{val}</span>
+    </div>
+  {/each}
+</div>
