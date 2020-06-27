3d896c9be1014d90a042e63f9e99dfd4db15ac87
Eric Rovell <ericrovell@gmail.com>
2020-06-27 23:46:01 +0300

    app

---
+++ src/ColorInput/ColorInput.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Input from "./Input.svelte";
+  import State from "./State.svelte";
+
+  import parseColorString from "../util/parse/parseColorString.js";
+
+  let userInput = "";
+
+  $: state = (!userInput.trim().length)
+    ? "empty"
+    : parseColorString(userInput);
+
+  const clearInput = () => userInput = "";
+</script>
+
+
+<Input bind:value={userInput} {clearInput} />
+<State {state} />
