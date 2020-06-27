e2d437cee1e53222cac5cb6975be8f97e02318ef
Eric Rovell <ericrovell@gmail.com>
2020-06-27 23:45:52 +0300

    state switch like component

---
+++ src/ColorInput/State.svelte
@@ -0,0 +1,28 @@
+<script>
+  // fade in/out container to switch between states
+  import ContainerFade from "@components/ContainerFade.svelte";
+
+  // possible user input state modes
+  import ColorInputTip from "@components/svg/ColorInputTip.svelte";
+  import StateValid from "./StateValid.svelte";
+  import StateInvalid from "./StateInvalid.svelte";
+
+  // user input prop
+  export let state = "";
+</script>
+
+<div>
+  {#if state === "empty"}
+    <ContainerFade>
+      <ColorInputTip />
+    </ContainerFade>    
+  {:else if state}
+    <ContainerFade>
+      <StateValid {...state} />
+    </ContainerFade>
+  {:else}
+    <ContainerFade>
+      <StateInvalid />
+    </ContainerFade>    
+  {/if}
+</div>
