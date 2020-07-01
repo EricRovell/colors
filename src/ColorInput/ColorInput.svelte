c2807bc665e9e1889b6e43eb0fe01c983bb06f58
Eric Rovell <ericrovell@gmail.com>
2020-07-01 13:27:25 +0300

    moved function to inline

--- src/ColorInput/ColorInput.svelte
+++ src/ColorInput/ColorInput.svelte
@@ -3,16 +3,18 @@
   import State from "./State.svelte";
 
   import parseColorString from "../util/parse/parseColorString.js";
+  import randomColorString from "@util/randColor/randomColorString.js";
 
   let userInput = "";
 
   $: state = (!userInput.trim().length)
     ? "empty"
     : parseColorString(userInput);
-
-  const clearInput = () => userInput = "";
 </script>
 
 
-<Input bind:value={userInput} {clearInput} />
+<Input
+  bind:value={userInput}
+  clearInput={() => userInput = ""}
+  randColorInput={() => userInput = randomColorString({ asString: true })} />
 <State {state} />
