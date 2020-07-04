81ef4b22540a6bbcff1ec60dab1843691c815642
Eric Rovell <ericrovell@gmail.com>
2020-07-04 12:13:27 +0300

    removed data transformation

--- src/util/parse/parseColorString.js
+++ src/util/parse/parseColorString.js
@@ -1,19 +1,18 @@
 import regex from "./regex.js";
-import modelData from "./modelData.js";
 
 export default function determineColorString(str) {
   // check every expression until 1st valid one
   for (let expression of regex) {
     let match = str.match(expression.expression);
     if (match) {
-      return modelData({
+      return {
         model: expression.model,
         type: expression.type,
-        match
-      });
+        values: match.groups
+      };
     }
   }
 
-  // none expression suited -> null
+  // none expression suited
   return null;
 }
