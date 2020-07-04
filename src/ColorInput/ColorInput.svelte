2dc0b9f7a3e5af0f968aa5db1aab508f3ddbd484
Eric Rovell <ericrovell@gmail.com>
2020-07-04 12:20:10 +0300

    simplified state and props

--- src/ColorInput/ColorInput.svelte
+++ src/ColorInput/ColorInput.svelte
@@ -2,19 +2,10 @@
   import Input from "./Input.svelte";
   import State from "./State.svelte";
 
-  import parseColorString from "../util/parse/parseColorString.js";
-  import randomColorString from "@util/randColor/randomColorString.js";
-
   let userInput = "";
-
-  $: state = (!userInput.trim().length)
-    ? "empty"
-    : parseColorString(userInput);
 </script>
-
 
 <Input
   bind:value={userInput}
-  clearInput={() => userInput = ""}
-  randColorInput={() => userInput = randomColorString({ asString: true })} />
-<State {state} />
+  clearInput={() => userInput = ""} />
+<State emptyInput={!userInput.trim().length} />
