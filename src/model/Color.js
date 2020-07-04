a31893bef077fe4b685a2e20acf485d5d4ff2d37
Eric Rovell <ericrovell@gmail.com>
2020-07-04 12:17:50 +0300

    Color class

--- src/model/Color.js
+++ src/model/Color.js
@@ -1,18 +1,56 @@
+import convert from "@util/convertion/Any.js";
+import parseColorString from "@util/parse/parseColorString.js";
+
+import ColorRGB from "./ColorRGB.js";
+import ColorHex from "./ColorHex.js";
+import ColorHSL from "./ColorHSL.js";
+import ColorHSV from "./ColorHSV.js";
+import ColorCMYK from "./ColorCMYK.js";
+
 export default class Color {
-  constructor({ hex, rgb, hsl, hsv, cmyk, name }) {
-    this.model = "hex";
-    // the main color is RGB under the hood
-    // the reason: any present model can be easily converted to RGB in one "step"
-    this.color = [ 0, 0, 0 ];
-    //
-    this.models = {
-      hex: this.hex,
-      rgb: this.rgb,
-      hsl: this.hsl,
-      hsv: this.hsv,
-      cmyk: this.cmyk,
-      name: this.name
+  constructor({ rgb, hex, hsl, hsv, cmyk, opacity = 1 } = {}) {
+    this.model = {
+      rgb: new ColorRGB(),
+      hex: new ColorHex(),
+      hsl: new ColorHSL(),
+      hsv: new ColorHSV(),
+      cmyk: new ColorCMYK()
     };
+    this.opacity = opacity;
+
+    // prop to check if the last mutation was valid
+    this.valid = false;
   }
+
+  colorFromString(string) {
+    // try to parse the string
+    const parseResult = parseColorString(string);
+    // no match -> no mutation, remember the failure in `valid` prop
+    if (!parseResult) {
+      this.valid = false;
+      return null;
+    };
+
+    // matched string, we know the model, it's type and object w/ values
+    const { model, type, values } = parseResult;
+
+    // set opacity if present
+    this.opacity = values?.opacity ?? 1;
+
+    // convert to other models
+    const colors = convert({
+      from: model,
+      value: values
+    });
+
+    // update props in all models
+    for (let model in colors) {
+      this.model[model].setValue(colors[model]);
+    }
+
+    // successful update
+    this.valid = true;
+    return true;
+  } 
   
 }
