389d849f7393c98adb3f1b626fc61bab3a61384d
Eric Rovell <ericrovell@gmail.com>
2020-07-04 12:21:14 +0300

    wip, tinkering with state

--- src/ColorInput/StateValid.svelte
+++ src/ColorInput/StateValid.svelte
@@ -2,24 +2,12 @@
   import ColorTable from "@components/ColorTable.svelte";
   import Opacity from "@components/Opacity.svelte";
 
-  export let model = null;
-  export let alpha = null;
-  export let value = null;
+  import Section from "@components/Section.svelte";
+
+  export let model;
 </script>
 
-<style>
-  div {
-    display: grid;
-    grid-template: 1fr / repeat(3, 1fr);
-    place-items: center;
-    column-gap: 1.5em;
-  }
-</style>
 
 <div>
-  <span>
-    {model.toUpperCase()}
-  </span>
-  <ColorTable {value} />
-  <Opacity opacity={alpha} />
+  <Section {model} />
 </div>
