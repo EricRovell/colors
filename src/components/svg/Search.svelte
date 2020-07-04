bf05d7e098b2903dfdc3c0716b05fae5172b92db
Eric Rovell <ericrovell@gmail.com>
2020-07-04 12:18:46 +0300

    search svg component

---
+++ src/components/svg/Search.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let searchColor;
+</script>
+
+<svg
+  viewBox="0 0 99.84 100"
+  xmlns="http://www.w3.org/2000/svg"
+  on:click={searchColor} >
+  <path d="M38.1,0A37.53,37.53,0,1,0,75.55,37.52,37.49,37.49,0,0,0,38.1,0Zm0,69.76A32.24,32.24,0,1,1,70.28,37.52,32.22,32.22,0,0,1,38.1,69.76Z" transform="translate(-0.65)"/>
+  <rect x="77.84" y="55.85" width="4.56" height="49.83" transform="translate(-34.29 80.31) rotate(-45)"/>
+</svg>
