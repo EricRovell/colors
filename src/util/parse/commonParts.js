203203b289ad1854a2bdd23788d95e372763fcd0
Eric Rovell <ericrovell@gmail.com>
2020-07-06 14:17:17 +0300

    more parts

--- src/util/parse/commonParts.js
+++ src/util/parse/commonParts.js
@@ -18,9 +18,14 @@ export default {
   range0to255: groupName => new RegExp(`(?<${groupName}>[01]?\\d\\d?|2[0-4]\\d|25[0-5])`),  
   range0to360: groupName => new RegExp(`(?<${groupName}>0|360|35\\d|3[0-4]\\d|[12]\\d\\d|0?\\d?\\d)`),
 
+  percentage: groupName => new RegExp(`(?<${groupName}>0|100|\\d{1,2})(?=%)`),
+
   /**
    * Hexadecimal characters
    */
   hex: groupName => new RegExp(`(?<${groupName}>[a-fA-F0-9])`),
-  hex2: groupName => new RegExp(`(?<${groupName}>[a-fA-F0-9]{2})`)
+  hex2: groupName => new RegExp(`(?<${groupName}>[a-fA-F0-9]{2})`),
+
+  separator: /(?:\s|\/|\-|\,)+/,
+  separatorOpt: /(?:\s|\/|\-|\,)*/,
 }
