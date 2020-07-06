c2d01154f87b9928d3fabc0f72edd8a6cb497a91
Eric Rovell <ericrovell@gmail.com>
2020-07-06 14:15:46 +0300

    validation; randomString getter; asArray getter

--- src/model/ColorHSL.js
+++ src/model/ColorHSL.js
@@ -1,5 +1,5 @@
 export default class ColorHSL {
-  constructor({ h, s, l, values } = { values: [ 0, 0, 0 ] }) {
+  constructor({ h = 0, s = 0, l = 0, values }) {
     
     if (values && Array.isArray(values)) {
       [ this.h, this.s, this.l ] = values;
@@ -13,6 +13,18 @@ export default class ColorHSL {
 
   setValue({ h, s, l }) {
     [ this.h, this.s, this.l ] = [ h, s, l ];
+  }
+
+  validate({ h, s, l }) {
+    [ h, s, l ] = [ h, s, l ].map(Number);
+
+    if (![ h, s, l ].every(value => Number.isInteger(value))) {
+      [ h, s, l ] = [ 0, 0, 0];
+    }
+
+    return {
+      h, s, l
+    };
   }
 
   get properties() {
@@ -35,11 +47,68 @@ export default class ColorHSL {
     ]
   }
 
+  get asArray() {
+    return [ this.h, this.s, this.l ];
+  }
+
   get toString() {
-    return `hsl ${this.h}deg ${this.s}% ${this.l}%`
+    return `${this.h} ${this.s} ${this.l}`;
   }
 
   get cssString() {
     return `hsl(${this.h}deg ${this.s}% ${this.l}%)`;
   }
+
+  get randomString() {
+    return "hsl" + [
+      Math.floor(Math.random() * 359),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100)
+    ].join(" ")
+  }
+
+  /* */
+
+  static modulo(x, n) {
+    return (x % n + n) % n;
+  }
+
+  rotateHue(angle) {
+    this.h = ColorHSL.modulo(this.h + angle, 360);
+    return this.h;
+  }
+
+  get complementary() {
+    return this.rotateHue(180);
+  }
+
+  /*
+  const getTriadic = color => {
+    const first = rotateHue(120);
+    const second = rotateHue(-120);
+    return [first(color), second(color)];
+  }
+  */
+
+  /*
+  const saturate = x => ({saturation, ...rest}) => ({
+  ...rest,
+  saturation: Math.min(1, saturation + x),
+  });
+  const desaturate = x => ({saturation, ...rest}) => ({
+    ...rest,
+    saturation: Math.max(0, saturation - x),
+  });
+  const lighten = x => ({lightness, ...rest}) => ({
+    ...rest,
+    lightness: Math.min(1, lightness + x)
+  });
+  const darken = x => ({lightness, ...rest}) => ({
+    ...rest,
+    lightness: Math.max(0, lightness - x)
+  });
+
+  const isGrayscale = ({saturation}) => saturation === 0;
+  const isDark = ({lightness}) => lightness < .5;
+*/
 }
