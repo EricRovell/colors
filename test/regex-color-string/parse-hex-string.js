470a0bdfdc58c1ab9b3631f67910f01dfcbc180f
Eric Rovell <ericrovell@gmail.com>
2020-07-06 23:55:20 +0300

    testing parsing hex color string

---
+++ test/regex-color-string/parse-hex-string.js
@@ -0,0 +1,64 @@
+import test from "ava";
+import ColorParser from "../../src/util/parse/parseColorString.js";
+
+test("Parse HEX string:", t => {
+
+  const parser = new ColorParser();
+
+  t.deepEqual(
+    parser.parseString("#1A3"),
+    [{
+      model: "hex",
+      type: "shorthand",
+      values: {
+        r: "1",
+        g: "A",
+        b: "3",
+        opacity: undefined
+      }
+    }]
+  );
+
+  t.deepEqual(
+    parser.parseString("#1A3C"),
+    [{
+      model: "hex",
+      type: "shorthand",
+      values: {
+        r: "1",
+        g: "A",
+        b: "3",
+        opacity: "C"
+      }
+    }]
+  );
+
+  t.deepEqual(
+    parser.parseString("#123ABC"),
+    [{
+      model: "hex",
+      type: undefined,
+      values: {
+        r: "12",
+        g: "3A",
+        b: "BC",
+        opacity: undefined
+      }
+    }]
+  );
+
+  t.deepEqual(
+    parser.parseString("#123ABC8F"),
+    [{
+      model: "hex",
+      type: undefined,
+      values: {
+        r: "12",
+        g: "3A",
+        b: "BC",
+        opacity: "8F"
+      }
+    }]
+  );
+
+});
