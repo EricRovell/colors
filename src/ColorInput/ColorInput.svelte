18297c504006cb61c1c932b531b7745f0681f020
Eric Rovell <ericrovell@gmail.com>
2020-07-06 14:22:55 +0300

    using parser

--- src/ColorInput/ColorInput.svelte
+++ src/ColorInput/ColorInput.svelte
@@ -1,11 +1,44 @@
 <script>
+  import { color } from "@stores/colorStore.js";
+
   import Input from "./Input.svelte";
   import State from "./State.svelte";
+  import Suggestions from "@components/Suggestions.svelte";
+  import Navigation from "@components/Navigation.svelte";
+
+  import ColorParser from "@util/parse/parseColorString.js";
+
+  const parser = new ColorParser();
 
   let userInput = "";
+  let suggestions = [];
+
+  $: emptyInput = !userInput.trim().length;
+
+  function makeSearch() {
+    if (event.key === "Enter") {
+      const result = parser.parseString(userInput);
+      if (result.length > 1) {
+        suggestions = result;
+      }
+      if (result.length === 1) {
+        color.setColor(...result);
+      }
+    }
+  }
 </script>
 
 <Input
   bind:value={userInput}
-  clearInput={() => userInput = ""} />
-<State emptyInput={!userInput.trim().length} />
+  {makeSearch}
+  clearInput={() => userInput = ""}
+  randInput={() => userInput = $color.model.cmyk.randomString}>
+    {#if !emptyInput}
+      <Navigation />
+      <Suggestions {suggestions} />
+    {/if}
+</Input>
+
+<State
+  emptyInput={emptyInput}
+/>
