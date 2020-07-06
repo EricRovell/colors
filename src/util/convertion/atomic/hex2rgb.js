10f3f2752c7a8c6b89a78965e0d65fead4e1a555
Eric Rovell <ericrovell@gmail.com>
2020-07-06 19:11:39 +0300

    fix: proper shorthand handling; fix string convert

--- src/util/convertion/atomic/hex2rgb.js
+++ src/util/convertion/atomic/hex2rgb.js
@@ -5,22 +5,30 @@
  */
 export default function hex2rgb({ r, g, b, values = [], string = "", asArray = false }) {
 
+  let red, green, blue;
+
   // get values from array if provided
   // or from named props
   // check if shorthand -> make full, ex: "123" -> "112233"
-  let [ red, green, blue ] = (Array.isArray(values) && values?.length)
-    ? values
-    : [ r, g, b ].map(value => (value.length === 1) ? value + value : value);
+  if (Array.isArray(values) && values?.length) {
+    [ red, green, blue ] = values
+      .map(value => value.length === 2 ? value : value + value);
+  }
 
   // from string -> break into array object
-  if (string) {
+  else if (string) {
     const hex = string.replace("#", "");
     // split hex string into channels
     [ red, green, blue ] = (hex.length === 3 || hex.length === 4)
-      ? hex.match(/.{1}/g)
+      ? hex.match(/.{1}/g).map(str => str + str)
       : (hex.length === 6 || hex.length === 8)
       ? hex.match(/.{2}/g)
       : [ 0, 0, 0 ];
+  }
+
+  else {
+    [ red, green, blue ] = [ r, g, b ]
+      .map(value => (value.length === 1) ? value + value : value);
   }
 
   const rgb = [ red, green, blue ].map(value => parseInt(value, 16));
