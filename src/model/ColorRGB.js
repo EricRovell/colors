5a8877003da4d96594319f4d2cae2be5feba3a63
Eric Rovell <ericrovell@gmail.com>
2020-07-06 14:15:30 +0300

    validation; randomString getter; asArray getter

--- src/model/ColorRGB.js
+++ src/model/ColorRGB.js
@@ -1,5 +1,5 @@
 export default class ColorRGB {
-  constructor({ r, g, b, type = "absolute", values } = { values: [ 0, 0, 0 ] }) {
+  constructor({ r = 0, g = 0, b = 0, type = "absolute", values }) {
     this.type = type;
 
     if (values && Array.isArray(values)) {
@@ -14,6 +14,22 @@ export default class ColorRGB {
 
   setValue({ r, g, b }) {
     [ this.r, this.g, this.b ] = [ r, g, b];
+  }
+
+  validate({ r, g, b }) {
+    const rgb = [ r, g, b ]
+      .map(Number)
+      .map(value => {
+        return (value >= 0 && value <= 255)
+          ? value
+          : 0
+      });
+
+    return {
+      r: rgb[0],
+      g: rgb[1],
+      b: rgb[2]
+    };
   }
 
   get properties() {
@@ -42,11 +58,23 @@ export default class ColorRGB {
     ]
   }
 
+  get asArray() {
+    return [ this.r, this.g, this.b ];
+  }
+
   get toString() {
-    return `rgb ${this.r} ${this.g} ${this.b}`
+    return `${this.r} ${this.g} ${this.b}`;
   }
 
   get cssString() {
     return `rgb(${this.r} ${this.g} ${this.b})`;
+  }
+
+  get randomString() {
+    const randRGB = new Array(3)
+      .fill()
+      .map(() => Math.floor(Math.random() * 255))
+
+    return `rgb ${randRGB.join(" ")}`;
   }
 }
