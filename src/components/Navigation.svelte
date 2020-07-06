7f03d587a7a8027ee4e4fa10b452b17f93fed2e3
Eric Rovell <ericrovell@gmail.com>
2020-07-06 14:20:39 +0300

    navigation component for sections

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,39 @@
+<script>
+  const sections = [ "Information", "Convertion" ];
+</script>
+
+<ol>
+  {#each sections as sectionName}
+    <li>
+      <a href={`#search-section-${sectionName.toLowerCase()}`}>
+        {sectionName}
+      </a>
+    </li>
+  {/each}
+</ol>
+
+<style>
+  ol {
+    list-style: none;
+
+    display: flex;
+    align-items: center;
+  }
+
+  li {
+    background: white;
+    padding: 0.1em 0.5em;
+    border-radius: 5px;
+    filter: drop-shadow(1px 1px 5px rgb(0 0 0 / 0.1));
+  }
+
+  li:not(:first-child) {
+    margin-left: 0.5em;
+  }
+
+  li:hover {
+    filter:
+      drop-shadow(1px 1px 5px rgb(0 0 0 / 0.1))
+      drop-shadow(1px 1px 10px rgb(0 0 0 / 0.25));
+  }
+</style>
