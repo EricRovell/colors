e937a9e3bc54d15a32e788b9c38f8cd24d4787db
Eric Rovell <ericrovell@gmail.com>
2020-07-08 13:41:22 +0300

    fixed opacity group; better separator

--- src/util/parse/commonParts.js
+++ src/util/parse/commonParts.js
@@ -8,17 +8,16 @@ export default {
    *   - Percentages in range [0, 100] with % symbol: 35%.
    * ! Percent values captured with % symbol
    */
-  opacity: /(?<opacity>0\.\d{1,2}|[1-9]\d{0,1}(?!\d)(?:\%)|100(?:\%)|0|1)/,
+  opacity: /(?: (?<opacity>0\.\d{1,2}|[1-9]\d?(?!\d)|100|0|1)?%?)?/,
 
   /**
    * Groups for capturing integer values in range defined range
    */
-  //range0to100: /(0|100|\d{1,2})/,
   range0to100: groupName => new RegExp(`(?<${groupName}>0|100|\\d{1,2})`),
   range0to255: groupName => new RegExp(`(?<${groupName}>[01]?\\d\\d?|2[0-4]\\d|25[0-5])`),  
   range0to360: groupName => new RegExp(`(?<${groupName}>0|360|35\\d|3[0-4]\\d|[12]\\d\\d|0?\\d?\\d)`),
 
-  percentage: groupName => new RegExp(`(?<${groupName}>0|100|\\d{1,2})(?=%)`),
+  percentage: groupName => new RegExp(`(?<${groupName}>0|100|\\d{1,2})(?:%)?`),
 
   /**
    * Hexadecimal characters
@@ -26,6 +25,5 @@ export default {
   hex: groupName => new RegExp(`(?<${groupName}>[a-fA-F0-9])`),
   hex2: groupName => new RegExp(`(?<${groupName}>[a-fA-F0-9]{2})`),
 
-  separator: /(?:\s|\/|\-|\,)+/,
-  separatorOpt: /(?:\s|\/|\-|\,)*/,
+  separator: /[\s\/]+/,
 }
