675adb4cb2d0491864619d033ac14712cd867c25
Eric Rovell <ericrovell@gmail.com>
2020-07-10 12:50:10 +0300

    taking all width of parent; centering

--- src/components/ContainerFade.svelte
+++ src/components/ContainerFade.svelte
@@ -3,8 +3,18 @@
 </script>
 
 <div
+  class="container"
   out:fade="{{ duration: 300 }}"
   in:fade="{{ delay: 300, duration: 300 }}">
   <slot />
 </div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+</style>
 
