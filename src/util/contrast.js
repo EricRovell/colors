9f38d0cd9b77bb30cc0cf3f1ed65ed8969279708
Eric Rovell <ericrovell@gmail.com>
2020-07-10 12:49:40 +0300

    fixed array value

--- src/util/contrast.js
+++ src/util/contrast.js
@@ -8,7 +8,7 @@ export default function contrast({ rgb: { r, g, b }, darkColor = "#000", lightCo
     }
   });
 
-  const lightness = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[3];
+  const lightness = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
 
   return (lightness > 0.179)
     ? darkColor
