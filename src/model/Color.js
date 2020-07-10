07a73d00e62f0e31666a77af000d48e0c00e3ba4
Eric Rovell <ericrovell@gmail.com>
2020-07-10 12:51:37 +0300

    finished constructor params flow

--- src/model/Color.js
+++ src/model/Color.js
@@ -7,19 +7,29 @@ import ColorHSV from "./ColorHSV.js";
 import ColorCMYK from "./ColorCMYK.js";
 
 export default class Color {
-  constructor({ rgb, hex, hsl, hsv, cmyk, opacity = 1 } = {}) {
+  constructor({ model, value } = {}) {
+    // get all color models
+    const data = Color.toAll({ model, value });
+
     this.model = {
-      rgb: new ColorRGB({ rgb }),
-      hex: new ColorHex({ hex }),
-      hsl: new ColorHSL({ hsl }),
-      hsv: new ColorHSV({ hsv }),
-      cmyk: new ColorCMYK({ cmyk })
+      rgb: new ColorRGB(data.rgb),
+      hex: new ColorHex(data.hex),
+      hsl: new ColorHSL(data.hsl),
+      hsv: new ColorHSV(data.hsv),
+      cmyk: new ColorCMYK(data.cmyk)
     };
-    this.opacity = opacity;
+    this.opacity = data?.value ?? 1;
+  }
+
+  static toAll({ model, type, value }) {
+    console.log(model);
+    return convert({
+      from: model,
+      value
+    });
   }
 
   set value({ model, type, values }) {
-    console.log(model, values);
     // set opacity if present
     this.opacity = values?.opacity ?? 1;
 
@@ -44,6 +54,23 @@ export default class Color {
     }
 
     return data;
+  }
+
+  get contrast() {
+    const rgb = this.model.rgb.asArray.map(value => {
+      const normalized = value / 255;
+      if (normalized <= 0.03928) {
+        return normalized / 12.92;
+      } else {
+        return ((normalized + 0.055) / 1.055) ** 2.4;
+      }
+    });
+  
+    const lightness = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+  
+    return (lightness > 0.179)
+      ? "#000"
+      : "#FFF";
   }
   
 }
