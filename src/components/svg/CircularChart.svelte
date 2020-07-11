5f4e038661f8425ffcfe8c21d87bf9cb566b999c
Eric Rovell <ericrovell@gmail.com>
2020-07-11 22:21:03 +0300

    using color prop

--- src/components/svg/CircularChart.svelte
+++ src/components/svg/CircularChart.svelte
@@ -1,16 +1,10 @@
 <script>
   export let text = "value";
   export let progress = 0;
+  export let color = "black";
 </script>
 
-<style>
-  svg {
-    width: 50px;
-    height: 50px;
-  }
-</style>
-
-<svg viewBox="0 0 36 36">
+<svg viewBox="0 0 36 36" style="--chart-progress:{progress}; --color:{color}">
   <!-- circle bg -->
   <path
     d="M18 2.0845
@@ -26,13 +20,24 @@
       a 15.9155 15.9155 0 0 1 0 31.831
       a 15.9155 15.9155 0 0 1 0 -31.831"
     fill="none"
-    stroke="#4CC790"
     stroke-width="2.8"
     stroke-linecap="round"
-    stroke-dasharray={`${progress}, 100`}
   />
   <!-- value -->
   <text x="18" y="20.35" fill="#666" font-size="0.5em" text-anchor="middle">
     {text}
   </text>
 </svg>
+
+<style>
+  svg {
+    width: 50px;
+    height: 50px;
+    transition: stroke-dasharray 0.15s linear;
+    stroke-dasharray: var(--chart-progress, 0) 100;
+  }
+
+  svg path {
+    stroke: var(--color, #4CC790);
+  }
+</style>
