35b342329f9665938110b5e793bd560a5c883c44
Eric Rovell <ericrovell@gmail.com>
2020-07-12 00:02:30 +0300

    using logo; temp app name

--- src/routes/ColorSearch.svelte
+++ src/routes/ColorSearch.svelte
@@ -1,10 +1,14 @@
 <script>
   import ColorInput from "../ColorInput/ColorInput.svelte";
+  import Logo from "@components/svg/Logo.svelte";
 </script>
 
 <main>
 	<div id="app">
-    <h1>Colours</h1>
+    <h1 class="title">
+      <Logo />
+      <span>Color Wind</span>
+    </h1>
     <ColorInput />
   </div>
 </main>
@@ -40,6 +44,18 @@
     margin-top: 4em;
     width: 100%;
     max-width: 640px;
+  }
+
+  .title {
+    display: grid;
+    place-items: center;
+    grid-auto-flow: column;
+    column-gap: 0.25em;
+  }
+
+  .title :global(svg) {
+    width: 0.75em;
+    height: 0.75em;
   }
 
 	h1 {
