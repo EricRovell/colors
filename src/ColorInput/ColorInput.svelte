556717045c3328f2171667c43c46d2c1c3b4c7fb
Eric Rovell <ericrovell@gmail.com>
2020-07-14 14:24:45 +0300

    using parser store; better search state management

--- src/ColorInput/ColorInput.svelte
+++ src/ColorInput/ColorInput.svelte
@@ -1,28 +1,29 @@
 <script>
   import { color } from "@stores/colorStore.js";
+  import { parser } from "@stores/parserStore.js";
 
   import Input from "./Input.svelte";
   import State from "./State.svelte";
-  import Suggestions from "@components/Suggestions.svelte";
   import Navigation from "@components/Navigation.svelte";
 
-  import ColorParser from "@util/parse/parseColorString.js";
-
-  const parser = new ColorParser();
-
   let userInput = "";
-  let suggestions = [];
 
-  $: emptyInput = !userInput.trim().length;
+  let state = "empty";
 
-  function makeSearch() {
-    if (event.key === "Enter") {
+  $: {
+    // check the input
+    if (!userInput.trim().length) {
+      state = "empty";
+    } else {
+      // parse user's input
+      // result is the array of possible color models
       const result = parser.parseString(userInput);
-      if (result.length > 1) {
-        suggestions = result;
-      }
-      if (result.length === 1) {
-        color.setColor(...result);
+
+      if (result.length) {
+        color.setColor(result[0]);
+        state = "valid";
+      } else {
+        state = null;
       }
     }
   }
@@ -30,15 +31,11 @@
 
 <Input
   bind:value={userInput}
-  {makeSearch}
   clearInput={() => userInput = ""}
-  randInput={() => userInput = $color.model.cmyk.randomString}>
-    {#if !emptyInput}
+  randInput={() => userInput = $color.randomColorString}>
+    {#if state !== "empty"}
       <Navigation />
-      <Suggestions {suggestions} />
     {/if}
 </Input>
 
-<State
-  emptyInput={emptyInput}
-/>
+<State state={state} />
