cb26bc0c6d3647a6d68c55aa7f973ada9f747150
Eric Rovell <ericrovell@gmail.com>
2020-07-16 16:30:38 +0300

    ThemeToggle, wip

---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Moon from "@components/svg/Moon.svelte";
+</script>
+
+<div>
+  <Moon />
+</div>
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+  }
+
+  div :global(svg) {
+    width: 1.5em;
+    height: 1.5em;
+  }
+</style>
