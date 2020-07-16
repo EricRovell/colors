3cba6d124817ba5da93f606c6244a652f0827205
Eric Rovell <ericrovell@gmail.com>
2020-07-16 16:30:30 +0300

    Footer

---
+++ src/layout/Footer.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Heart from "@components/svg/Heart.svelte";
+</script>
+
+<footer>
+  <div class="container">
+    <span>
+      Color Wind v.0.2
+    </span>
+    <div class="author">
+      <span>Made with</span>
+      <Heart />
+      <a href="https://github.com/EricRovell">
+        by Eric Rovell
+      </a>
+    </div>
+  </div>
+</footer>
+
+<style>
+  footer {
+    padding: 0.75em 1.25em;
+  }
+
+  footer :global(svg) {
+    width: 1em;
+    height: 1em;
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 640px;
+    margin: auto;
+  }
+
+  .author {
+    display: grid;
+    grid-auto-flow: column;
+    column-gap: 0.25em;
+    place-items: center;
+  }
+
+</style>
