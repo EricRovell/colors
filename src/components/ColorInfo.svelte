34b2f93961e73115cf2231bd7922fc6031380c63
Eric Rovell <ericrovell@gmail.com>
2020-07-16 16:32:40 +0300

    getting data for percentage values desc

--- src/components/ColorInfo.svelte
+++ src/components/ColorInfo.svelte
@@ -3,25 +3,40 @@
 
   import Color from "@components/Color.svelte";
   import ColorTable from "@components/ColorTable.svelte";
+
+  $: rgb = $color.rgb;
+  $: cmyk = $color.cmyk;
 </script>
 
 <div class="container">
-  <div>
+  <div class="color-container">
     <Color data={$color.data} />
   </div>
-  <ColorTable data={$color.rgb.properties} />
-  <ColorTable data={$color.cmyk.properties} />
+  <div class="description">
+    <p>{`In RGB color space, this color is composed of ${rgb.percentage.r}% red, ${rgb.percentage.g}% green, and ${rgb.percentage.b}% blue.`}</p>
+    <ColorTable data={rgb.properties} />
+    <p>{`In CMYK color space, this color is composed of ${cmyk.percentage.c}% cyan, ${cmyk.percentage.m}% magenta, ${cmyk.percentage.y}% yellow, and ${cmyk.percentage.k}% black.`}</p>
+    <ColorTable data={cmyk.properties} />
+    <p>{`It has a hue angle of 0 degrees, saturation of 5% and a lightness of 78%.`}</p>
+  </div>
 </div>
 
 <style>
   .container {
     display: grid;
-    grid-template: 1fr 1fr / 1fr 1fr;
-    column-gap: 20px;
+    grid-template: auto / 135px 1fr;
+    place-items: center;
+    column-gap: 1.5em;
+
+    font-family: "Inconsolata", sans-serif;
   }
 
-  div {
-    grid-row: 1 / span 2;
-    grid-column: 1;
+  .color-container {
+    align-self: start;
+  }
+
+  .description {
+    display: grid;
+    row-gap: 0.85em;
   }
 </style>
