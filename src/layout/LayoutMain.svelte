431157480c99e34af6a4714025cf67eefcb703f1
Eric Rovell <ericrovell@gmail.com>
2020-07-16 16:31:08 +0300

    LayoutMain for app

---
+++ src/layout/LayoutMain.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Header from "@src/layout/Header.svelte";
+  import Footer from "@src/layout/Footer.svelte";
+</script>
+
+<main id="layout-main">
+  <Header />
+  <div class="content">
+    <slot />
+  </div>
+  <Footer />
+</main>
+
+<style>
+  #layout-main {
+    width: 100%;
+
+    background: var(--bg);
+  }
+
+  .content {
+    display: grid;
+    justify-items: center;
+    width: 100%;
+    min-height: 150vh;
+  }
+</style>
