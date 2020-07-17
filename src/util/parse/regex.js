330263649b8d540df6fac571e0237bd611999880
Eric Rovell <ericrovell@gmail.com>
2020-07-17 15:03:59 +0300

    using Map

--- src/util/parse/regex.js
+++ src/util/parse/regex.js
@@ -10,52 +10,103 @@ const {
   separator
 } = commonParts;
 
-export default [
+const models = new Map();
 
-  // HEX
-  // Description: HEX shorthand notation; hash symbol is optional.
-  // Examples: #123, 123
-  // Groups: (R)(G)(B) 
-  {
-    model: "hex",
-    type: "shorthand",
-    expression: compileRegExp({
-      parts: [ /^\#?/, hex("r"), hex("g"), hex("b"), hex("opacity"), "?", /$/ ],
-      flags: "i"
-    })
-  },
+/**
+ * Model: HEX, shorthand
+ * Remarks: # is optional.
+ * Examples: #1A3, 1A3
+ * Groups: (r)(g)(b)(?a)
+*/
+models.set(
+  compileRegExp({ parts: [ /^\#?/, hex("r"), hex("g"), hex("b"), hex("opacity"), "?", /$/ ] }),
+  "hex"
+);
 
-  // Description: HEX notation; hash symbol is optional.
-  // Examples: #123ABC, 123ABC
-  // Groups: (RR)(GB)(BB)
-  {
-    model: "hex",
-    expression: compileRegExp({
-      parts: [ /^\#?/, hex2("r"), hex2("g"), hex2("b"), hex2("opacity"), "?", /$/ ],
-      flags: "i"
-    })
-  },
+/**
+ * Model: HEX
+ * Remarks: # is optional.
+ * Examples: #1A3D45, 1A3D45
+ * Groups: (rr)(gg)(bb)(?aa)
+*/
+models.set(
+  compileRegExp({ parts: [ /^\#?/, hex2("r"), hex2("g"), hex2("b"), hex2("opacity"), "?", /$/ ] }),
+  "hex"
+);
 
-  // RGB
+/**
+ * Model: RGB
+ * Remarks: model indicator and parentheses are optional.
+ * Examples:
+ *   255 135 55
+ *   255, 135, 55
+ *   (255 135 55)
+ *   rgb(255 135 55)
+ *   rgb(255, 135, 55)
+ *   255 135 55 28
+ *   255, 135, 55 0.28
+ *   (255 135 55 98)
+ *   rgb(255 135 55 0.98)
+ *   rgb(255, 135, 55)
+ * Groups: (r)(g)(b)(?opacity)
+*/
+models.set(
+  compileRegExp({ parts: [ /^(?:rgb|rgba?)?[\s\/]*\(?[\s\/]*/, range0to255("r"), separator, range0to255("g"), separator, range0to255("b"), opacity, /\)?$/ ] }),
+  "rgb"
+);
 
-  // Description: RGB; rgb function identificator and parenthesis are optional.
-  // Examples:
-  //   - 255 135 55
-  //   - 255, 135, 55
-  //   - (255 135 55)
-  //   - rgb(255 135 55)
-  //   - rgb(255, 135, 55)
-  // Groups: (R)(G)(B)
-  {
-    model: "rgb",
-    type: "absolute",
-    expression: compileRegExp({
-      parts: [ /^(?:rgb|rgba?)?[\s\/]*\(?[\s\/]*/, range0to255("r"), separator, range0to255("g"), separator, range0to255("b"), opacity, /\)?$/ ],
-      flags: "i"
-    })
-  },
+/**
+ * Model: HSL
+ * Remarks: model indicator, percentage symbol, and parentheses are optional.
+ * Examples:
+ *   360 25% 35%
+ *   360deg 25% 35%
+ *   360, 25, 35
+ *   360deg, 25%, 35%
+ *   hsl(360 25% 35%)
+ *   hsl(360deg 25% 35%)
+ *   hsl(360, 25%, 35%)
+ *   hsl(360deg, 25%, 35%)
+ * Groups: (h)(s)(l)(?opacity)
+*/
+models.set(
+  compileRegExp({ parts: [ /^(?:hsl|hsla)?[\s\/]*\(?[\s\/]*/, range0to360("h"), /(?:deg)?/, separator, percentage("s"), separator, percentage("l"), opacity, /\)?$/ ] }),
+  "hsl"
+);
 
-  // Description: RGB with percentage values; rgb function identificator and parenthesis are optional, percent symbol is required.
+/**
+ * Model: HSV / HSB
+ * Remarks: model indicator, percentage symbol, and parentheses are optional.
+ * Examples: same as HSL, only possible to distinguish by model identificator
+ * Groups: (h)(s)(v)(?opacity)
+*/
+models.set(
+  compileRegExp({ parts: [ /^(?:hsv|hsva|hsb|hsba)?[\s\/]*\(?[\s\/]*/, range0to360("h"), /(?:deg)?/, separator, percentage("s"), separator, percentage("v"), opacity, /\)?$/ ] }),
+  "hsv"
+);
+
+/**
+ * Model: CMYK
+ * Remarks: model indicator, percentage symbol, and parentheses are optional.
+ * Examples:
+ *   cmyk 25 35 45 79
+ *   cmyk 25% 35% 45% 79%
+ *   25 35 45 79
+ *   25% 35% 45% 79%
+ *   25 35 45 79 25%
+ *   25% 35% 45% 79% 0.39
+ * Groups: (h)(s)(l)(?opacity)
+*/
+models.set(
+  compileRegExp({ parts: [ /^(?:cmyk)?[\s\/]*\(?[\s\/]*/, percentage("c"), separator, percentage("m"), separator, percentage("y"), separator, percentage("k"), opacity, /\)?$/ ] }),
+  "cmyk"
+);
+
+
+export default models;
+
+
+// Description: RGB with percentage values; rgb function identificator and parenthesis are optional, percent symbol is required.
   // Examples:
   //   - 85% 32% 22%
   //   - 85%, 32%, 22%
@@ -63,60 +114,7 @@ export default [
   //   - rgb(85% 32% 22%)
   //   - rgb(85%, 32%, 22%)
   // Groups: (R)(G)(B)
-  {
-    model: "rgb",
-    type: "percentage",
-    expression: compileRegExp({
-      parts: [ /^(?:rgb|rgba?)?[\s\/]*\(?[\s\/]*/, percentage("r"), separator, percentage("g"), separator, percentage("b"), opacity, /\)?$/ ],
-      flags: "i"
-    })
-  },
-
-  // HSL
-
-  // Description: HSL; function identificator, parenthesis, and deg are optional.
-  // Examples:
-  //   - 360 25% 35%
-  //   - 360deg 25% 35%
-  //   - 360, 25%, 35%
-  //   - 360deg, 25%, 35%
-  //   - hsl(360 25% 35%)
-  //   - hsl(360deg 25% 35%)
-  //   - hsl(360, 25%, 35%)
-  //   - hsl(360deg, 25%, 35%)
-  // Groups: (H)(S)(L)
-  {
-    model: "hsl",
-    expression: compileRegExp({
-      parts: [ /^(?:hsl|hsla)?[\s\/]*\(?[\s\/]*/, range0to360("h"), /(?:deg)?/, separator, percentage("s"), separator, percentage("l"), opacity, /\)?$/ ],
-      flags: "i"
-    })
-  },
-
-  // Description: HSV; function identificator, parenthesis, and deg are optional.
-  // Examples:
-  // Groups: (H)(S)(V)
-  {
-    model: "hsv",
-    expression: compileRegExp({
-      parts: [ /^(?:hsv|hsva|hsb|hsba)?[\s\/]*\(?[\s\/]*/, range0to360("h"), /(?:deg)?/, separator, percentage("s"), separator, percentage("v"), opacity, /\)?$/ ],
-      flags: "i"
-    })
-  },
-
-  // CMYK
-
-  // Description: 
-  // Examples:
-  //   - 360 25% 35%
-  // Groups: (C)(M)(Y)(K)
-  {
-    model: "cmyk",
-    props: "percentage",
-    expression: compileRegExp({
-      parts: [ /^(?:cmyk)?[\s\/]*\(?[\s\/]*/, percentage("c"), separator, percentage("m"), separator, percentage("y"), separator, percentage("k"), opacity, /\)?$/ ],
-      flags: "i"
-    })
-  },
-
-];
+//models.set(
+  //compileRegExp({ parts: [ /^(?:rgb|rgba?)?[\s\/]*\(?[\s\/]*/, percentage("r"), separator, percentage("g"), separator, percentage("b"), opacity, /\)?$/ ] }),
+  //"rgb, %"
+//);
