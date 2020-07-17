b974e9b19b7d602799fb030eab86ffb333084422
Eric Rovell <ericrovell@gmail.com>
2020-07-17 15:03:51 +0300

    default flag value

--- src/util/parse/compileRegExp.js
+++ src/util/parse/compileRegExp.js
@@ -5,7 +5,7 @@
  * @param {string} params.flags - Flags to include in merged regular expression
  * @returns {RegExp} Merged reugular expression literal
  */
-export default function compileRegExp({ parts, flags }) {
+export default function compileRegExp({ parts, flags = "i" }) {
   return new RegExp(
     parts.reduce((acc, expression) => {
       return (expression instanceof RegExp)
