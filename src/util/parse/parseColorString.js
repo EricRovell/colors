f381696ed3b9fdc309c3e285655a068c1b3e473b
Eric Rovell <ericrovell@gmail.com>
2020-07-17 16:57:43 +0300

    parsing named css colors functinality

--- src/util/parse/parseColorString.js
+++ src/util/parse/parseColorString.js
@@ -1,9 +1,12 @@
 import { writable } from "svelte/store";
+
 import regex from "./regex.js";
+import namedColors from "./named-css-colors.js";
 
 export default class ColorParser {
   constructor() {
     this.expressions = regex;
+    this.colorNames = namedColors;
     this.history = writable([]);
     this.query = writable([]);
     this.limit = 15;
@@ -12,12 +15,47 @@ export default class ColorParser {
   }
 
   parseString(string) {
+    console.log(string);
+    // check for named color
+    const named = this.checkForName(string);
+
+    let match = (named)
+      ? [ named ]
+      : this.parseWithRegex(string)
+    
+
+    if (match.length) {
+      this.history.update(value => [ ...value, match ]);
+      this.query.set(match);
+    }
+
+    return match;
+  }
+
+  checkForName(string) {
+    const hex = this.colorNames[string.toUpperCase()]
+    if (hex) {
+      return {
+        model: "hex",
+        value: {
+          r: hex.slice(1, 3),
+          g: hex.slice(3, 5),
+          b: hex.slice(5)
+        }
+      };
+    }
+
+    return false;
+  }
+
+  parseWithRegex(string) {
     const matches = [];
 
+    // parse with regex
     for (let [ expression, model ] of this.expressions.entries()) {
       let match = string.match(expression);
       if (match) {
-
+        // hex has string values, others are numeral
         const value = (model === "hex")
           ? match.groups
           : ColorParser.processGroups(match.groups);
@@ -29,14 +67,7 @@ export default class ColorParser {
       }
     }
 
-    if (matches.length) {
-      this.history.update(value => [ ...value, matches ]);
-      this.query.set(matches);
-
-      return matches
-    }
-
-    return [];
+    return matches;
   }
 
   static processGroups({ opacity, ...rest }) {
