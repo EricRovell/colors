b5eabbaf1e2343c5e4607e04562c4774d463b843
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:06:03 +0300

    ColorShades from store

---
+++ src/components/ColorShades.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { color } from "@stores/colorStore.js";
+  import Palette from "@components/Palette.svelte";
+
+  $: shades = {
+    model: "hsl",
+    values: $color.generateShades(10)
+  };
+</script>
+
+<Palette colors={shades} />
