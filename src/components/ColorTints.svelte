0a77d32d555959838f0a0e2048a3cd73258c2cca
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:05:55 +0300

    ColorTints from store

---
+++ src/components/ColorTints.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { color } from "@stores/colorStore.js";
+  import Palette from "@components/Palette.svelte";
+
+  $: tints = {
+    model: "hsl",
+    values: $color.generateTints(10)
+  };
+</script>
+
+<Palette colors={tints} />
