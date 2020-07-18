799b2c4e9927ce268c0157f5e95025c32f5b55d1
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:03:14 +0300

    separated values from parameters; accepting object and array

--- src/util/convertion/atomic/hex2rgb.js
+++ src/util/convertion/atomic/hex2rgb.js
@@ -1,35 +1,40 @@
 /**
- * Converts HEX/HEXA color string to RGB/RGBA model values.
- * @param {string|string[]|number[]|object} hex - The HEX/HEXA color model values. Hash symbol is optional.
- * @returns {Number[]} an array containing RGB model values in range [0, 255]
+ * Converts HEX color to RGB model values.
+ * @param {Number[]|object|string} values - The HSL color model values array, object, or string.
+ * @param {object} [parameters] - Parameters object.
+ * @param {boolean} [parameters.asArray=false] - Return result as array. 
+ * @returns {object|Number[]} an array containing RGB model values.
  */
-export default function hex2rgb({ r, g, b, values = [], string = "", asArray = false }) {
+export default function hex2rgb(values, { asArray = false } = {}) {
 
-  let red, green, blue;
+  let hex;
 
-  // get values from array if provided
-  // or from named props
-  // check if shorthand -> make full, ex: "123" -> "112233"
-  if (Array.isArray(values) && values?.length) {
-    [ red, green, blue ] = values
-      .map(value => value.length === 2 ? value : value + value);
-  }
-
-  // from string -> break into array object
-  else if (string) {
-    const hex = string.replace("#", "");
+  // from array
+  if (Array.isArray(values)) {
+    if (values.length === 3) {
+      hex = values;
+    } else if (values.length === 1) {
+      hex = new Array(3).fill(values[0]);
+    } else {
+      hex = [ 0, 0, 0 ];
+    }
+  // from string
+  } else if (typeof values === "string") {
+    hex = values.replace("#", "");
     // split hex string into channels
-    [ red, green, blue ] = (hex.length === 3 || hex.length === 4)
+    hex = (hex.length === 3 || hex.length === 4)
       ? hex.match(/.{1}/g).map(str => str + str)
       : (hex.length === 6 || hex.length === 8)
       ? hex.match(/.{2}/g)
       : [ 0, 0, 0 ];
+  // from object
+  } else {
+    hex = [ values.r, values.g, values.b ];
   }
 
-  else {
-    [ red, green, blue ] = [ r, g, b ]
-      .map(value => (value.length === 1) ? value + value : value);
-  }
+  // check if shorthand -> make full, ex: "123" -> "112233"
+  const [ red, green, blue ] = hex
+    .map(value => value.length === 2 ? value : value + value);
 
   const rgb = [ red, green, blue ].map(value => parseInt(value, 16));
 
