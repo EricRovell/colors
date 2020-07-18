7ffba125bda1748fd37bbd73f1b695d695da0a21
Eric Rovell <ericrovell@gmail.com>
2020-07-18 21:58:26 +0300

    using new routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,19 @@
 // https://github.com/ItalyPaleAle/svelte-spa-router
 
 import ColorSearch from "./ColorSearch.svelte";
-
-import NamedRoute from "@routes/NamedRoute.svelte";
+import Help from "@routes/Help.svelte";
+import About from "@routes/About.svelte";
+import ColorPage from "@routes/ColorPage.svelte";
+import PalettePage from "@routes/PalettePage.svelte";
 
 export default {
   // exact
   "/": ColorSearch,
+  "/help": Help,
+  "/about": About,
 
   // with named params
   // ex: /#/rgb/12,12,12
-  "/:model/:values": NamedRoute
+  "/color/:model/:values": ColorPage,
+  "/palette/:model/:values": PalettePage
 };
