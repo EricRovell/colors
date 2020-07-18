7b9063792b2212f5ce7e675f2e5e261b891ea74d
Eric Rovell <ericrovell@gmail.com>
2020-07-18 21:58:41 +0300

    moved layout out

--- src/routes/ColorSearch.svelte
+++ src/routes/ColorSearch.svelte
@@ -1,17 +1,8 @@
 <script>
-  //import Header from "@src/layout/Header.svelte";
-  import LayoutMain from "@src/layout/LayoutMain.svelte";
   import ColorInput from "../ColorInput/ColorInput.svelte";
+  import FadingWrapper from "@src/layout/FadingWrapper.svelte";
 </script>
 
-<LayoutMain>
+<FadingWrapper>
   <ColorInput />
-</LayoutMain>
-
-<style>
-  :global(:root *) {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-</style>
+</FadingWrapper>
