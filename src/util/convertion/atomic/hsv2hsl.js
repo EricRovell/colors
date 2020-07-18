9625d01e9bea3a194ad2394658706f72d97ea093
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:03:01 +0300

    separated values from parameters; accepting object and array

--- src/util/convertion/atomic/hsv2hsl.js
+++ src/util/convertion/atomic/hsv2hsl.js
@@ -1,16 +1,23 @@
 /**
- * Converts HSV color string to HSL model values.
- * @param {Number[]} hsv - The HSV color model values array.
- * @param {Number} hsv[].hue - hue value in range [0, 360].
- * @param {Number} hsv[].saturation - saturation value in range [0, 1].
- * @param {Number} hsv[].value - value value in range [0, 1].
- * @returns {Number[]} an array containing HSL model values in range [hue:[0, 360], saturation: [0, 1], lightness: [0, 1]].
+ * Converts HSV/HSB color to HSL model values.
+ * @param {Number[]|object} values - The HSV color model values array or object with channel as keys.
+ * @param {object} [parameters] - Parameters object.
+ * @param {boolean} [parameters.asArray=false] - Return result as array. 
+ * @returns {object|Number[]} an array containing HSL model values.
  */
-export default function hsv2hsl({ h, s, v, values = [], asArray = false }) {
+export default function hsv2hsl(values, { asArray = false } = {}) {
 
-  const [ hue, saturationV, value ] = (Array.isArray(values) && values?.length)
-    ? values
-    : [ h, s / 100, v / 100 ];
+  let hsv;
+
+  if (Array.isArray(values)) {
+    hsv = (values.length === 3)
+      ? values
+      : [ 0, 0, 0 ];
+  } else {
+    hsv = [ values.h, values.s, values.v ];
+  }
+
+  const [ hue, saturationV, value ] = [ hsv[0], hsv[1] / 100, hsv[2] / 100 ];
   
   const lightness = value * (1 - saturationV / 2);
   const saturationL = (lightness === 0 || lightness === 1)
