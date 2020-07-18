ea12114001a66ddf1212e189b8ff4e327e114864
Eric Rovell <ericrovell@gmail.com>
2020-07-18 21:59:58 +0300

    using wrapper for future mobile layout styling

--- src/components/ColorPalettes.svelte
+++ src/components/ColorPalettes.svelte
@@ -3,20 +3,30 @@
   import Palette from "@components/Palette.svelte";
 
   let palettes = $color.constructor.availablePalettes;
-
-  let x = ["triadic"];
-
-  console.log(x.length);
 </script>
 
-{#each palettes as palette}
-  <div>
-    <span>{palette}</span>
-    <Palette colors={{
-      model: "hsl",
-      values: $color.getPalette(palette)
-    }} />
-  </div>
-{:else}
-  <p>Something is wrong...</p>
-{/each}
+<div class="wrapper">
+  {#each palettes as palette}
+    <div class="palette-container">
+      <span>{palette}</span>
+      <Palette colors={{
+        model: "hsl",
+        values: $color.getPalette(palette)
+      }} />
+    </div>
+  {:else}
+    <p>Something is wrong...</p>
+  {/each}
+</div>
+
+<style>
+  .wrapper {
+    display: grid;
+    row-gap: 1.5em;
+  }
+
+  .palette-container {
+    display: grid;
+    row-gap: 0.85em;
+  }
+</style>
