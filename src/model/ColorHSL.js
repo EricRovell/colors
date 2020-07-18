d86ed0d8690b6bd4449bb8633ab5ba89a422ca36
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:04:13 +0300

    simplified constructor; tints; shades; palettes

--- src/model/ColorHSL.js
+++ src/model/ColorHSL.js
@@ -1,8 +1,8 @@
 export default class ColorHSL {
-  constructor({ h = 0, s = 0, l = 0 }) {
-    this.h = +h;
-    this.s = +s;
-    this.l = +l;
+  constructor(values) {
+    [ this.h, this.s, this.l ] = (Array.isArray(values))
+      ? values
+      : [ values.h, values.s, values.l ];
   }
 
   get data() {
@@ -72,29 +72,111 @@ export default class ColorHSL {
     ];
   }
 
-  /* */
+  generateShades(shades) {
+    const colors = [];
+    const step = Math.ceil(50 / shades);
+    for (let lightness = 50; lightness >= 0; lightness -= step) {
+      colors.push(
+        [ this.h, this.s, lightness ]
+      );
+    }
+
+    return colors;
+  }
+
+  generateTints(tints) {
+    const colors = [];
+    const step = Math.ceil(50 / tints);
+    for (let lightness = 50; lightness <= 100; lightness += step) {
+      colors.push(
+        [ this.h, this.s, lightness ]
+      );
+    }
+
+    return colors;
+  }
 
   static modulo(x, n) {
     return (x % n + n) % n;
   }
 
   rotateHue(angle) {
-    this.h = ColorHSL.modulo(this.h + angle, 360);
-    return this.h;
+    return ColorHSL.modulo(this.h + angle, 360);
+  }
+
+  get analogous() {
+    return [
+      [ this.rotateHue(30), this.s, this.l ],
+      [ this.h, this.s, this.l ],
+      [ this.rotateHue(330), this.s, this.l ]
+    ];
   }
 
   get complementary() {
-    return this.rotateHue(180);
+    return [
+      [ this.h, this.s, this.l ],
+      [ this.rotateHue(180), this.s, this.l ]
+    ];
   }
 
-  /*
-  const getTriadic = color => {
-    const first = rotateHue(120);
-    const second = rotateHue(-120);
-    return [first(color), second(color)];
+  get splitComplementary() {
+    return [
+      [ this.rotateHue(150), this.s, this.l ],
+      [ this.h, this.s, this.l ],
+      [ this.rotateHue(210), this.s, this.l ]
+    ];
   }
-  */
 
+  get triadic() {
+    return [
+      [ this.rotateHue(120), this.s, this.l ],
+      [ this.h, this.s, this.l ],
+      [ this.rotateHue(240), this.s, this.l ]
+    ];
+  }
+
+  get tetradic() {
+    return [
+      [ this.h, this.s, this.l ],
+      [ this.rotateHue(120), this.s, this.l ],  
+      [ this.rotateHue(180), this.s, this.l ],
+      [ this.rotateHue(300), this.s, this.l ]
+    ];
+  }
+
+  get square() {
+    return [
+      [ this.h, this.s, this.l ],
+      [ this.rotateHue(90), this.s, this.l ],  
+      [ this.rotateHue(180), this.s, this.l ],
+      [ this.rotateHue(270), this.s, this.l ]
+    ];
+  }
+
+  get monochromatic() {
+    const colors = [];
+    let start, end;
+    let range = 15;
+
+    let [ left, right ] = [ this.l - range, this.l + range ]
+
+    if (left > 0 && right < 100) {
+      [ start, end ] = [ left, right ]
+    } else if (left < 0) {
+      [ start, end ] = [ 0, 50 ];
+    } else if (right > 100) {
+      [ start, end ] = [ 50, 85 ];
+    }
+
+    const step = Math.floor((end - start) / 5);
+
+    for (let lightness = start; lightness <= end; lightness += step) {
+      colors.push([ this.h, this.s, lightness ]);
+    }
+
+    return colors;
+  }
+  
   /*
   const saturate = x => ({saturation, ...rest}) => ({
   ...rest,
@@ -112,8 +194,5 @@ export default class ColorHSL {
     ...rest,
     lightness: Math.max(0, lightness - x)
   });
-
-  const isGrayscale = ({saturation}) => saturation === 0;
-  const isDark = ({lightness}) => lightness < .5;
 */
 }
