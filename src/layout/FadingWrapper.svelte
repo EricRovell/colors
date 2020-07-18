464ef18e097cfb12d0e7002ddb04e8b17ef0062a
Eric Rovell <ericrovell@gmail.com>
2020-07-18 21:56:28 +0300

    wrapper for route change transitions

---
+++ src/layout/FadingWrapper.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { fade } from "svelte/transition";
+</script>
+
+<div in:fade="{{ duration: 500 }}">
+  <slot />
+</div>
+
+<style>
+  div {
+    width: 100%;
+    height: 100%;
+    max-width: 640px;
+    display: flex;
+    justify-content: center;
+  }
+
+  @media screen and (max-width: 520px) {
+    div {
+      padding: 0 1em;
+    }
+  }
+</style>
