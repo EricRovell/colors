91a671c2c574db5cbc14621a7514eecf6d562131
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:06:51 +0300

    using new sections

--- src/ColorInput/StateValid.svelte
+++ src/ColorInput/StateValid.svelte
@@ -4,26 +4,40 @@
   import Suggestions from "@components/Suggestions.svelte";
   import ColorModelValues from "@components/ColorModelValues.svelte";
   import ColorInfo from "@components/ColorInfo.svelte";
+  import ColorPreview from "@components/ColorPreview.svelte";
+  import ColorShades from "@components/ColorShades.svelte";
+  import ColorTints from "@components/ColorTints.svelte";
+  import ColorPalettes from "@components/ColorPalettes.svelte";
 </script>
 
 <div class="container">
-  
-    <div class="section-wrapper">
-      <Suggestions />
-    </div>
-
+  <div class="section-wrapper">
+    <Suggestions />
+  </div>
   <Section name="Information">
     <ColorInfo />
   </Section>
   <Section name="Convertion">
     <ColorModelValues />
   </Section>
+  <Section name="Preview">
+    <ColorPreview />
+  </Section>
+  <Section name="Shades">
+    <ColorShades />
+  </Section>
+  <Section name="Tints">
+    <ColorTints />
+  </Section>
+  <Section name="Palettes">
+    <ColorPalettes />
+  </Section>
 </div>
 
 <style>
   .container {
     display: grid;
-    row-gap: 1em;
+    row-gap: 1.5em;
     width: 100%;
   }
 
