53b22fa0dae6ca81419790f5ab69dc20cfef38c9
Eric Rovell <ericrovell@gmail.com>
2020-07-18 18:06:32 +0300

    Palettes section for store

---
+++ src/components/ColorPalettes.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { color } from "@stores/colorStore.js";
+  import Palette from "@components/Palette.svelte";
+
+  let palettes = $color.constructor.availablePalettes;
+
+  let x = ["triadic"];
+
+  console.log(x.length);
+</script>
+
+{#each palettes as palette}
+  <div>
+    <span>{palette}</span>
+    <Palette colors={{
+      model: "hsl",
+      values: $color.getPalette(palette)
+    }} />
+  </div>
+{:else}
+  <p>Something is wrong...</p>
+{/each}
