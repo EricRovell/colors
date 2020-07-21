3af331621a7a60bae02c997c41a255553bf0eb86
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:21:01 +0300

    ColorLink lightweight component

---
+++ src/components/ColorLink.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let color;
+</script>
+
+<div class="color" style="--color: {color.cssString};">
+  <a href="hi">
+    <span>{color.model}</span>
+    <div>
+      {#each color.asArray as value}
+        <span>{value}</span>
+      {/each}
+    </div>
+  </a>
+</div>
+
+<style>
+  .color {
+    --size: 100px;
+
+    width: var(--size);
+    height: var(--size);
+
+    background: var(--color);
+    transition: background 0.15s linear;
+    
+  }
+</style>
