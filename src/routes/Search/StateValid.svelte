5a8212ed942da4a9e922432597da21149057da2e
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:18:10 +0300

    using Properties unified color props component

---
+++ src/routes/Search/StateValid.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { fade } from "svelte/transition";
+
+  import Suggestions from "@components/Suggestions.svelte";
+  import Properties from "@components/color-properties/Properties.svelte";
+</script>
+
+<div class="container" in:fade="{{ duration: 500 }}">
+  <div class="section-wrapper">
+    <Suggestions />
+  </div>
+  <Properties />
+</div>
+
+<style>
+  .container {
+    display: grid;
+    row-gap: 1.5em;
+    width: 100%;
+  }
+
+  .section-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5em;
+    width: 100%;
+  }
+</style>
