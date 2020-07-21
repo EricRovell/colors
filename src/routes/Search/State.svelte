e7e04f83018b76984a4833cda0e6f692739d5c1f
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:19:17 +0300

    using component directive

---
+++ src/routes/Search/State.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { color } from "@stores/colorStore.js";
+
+  // fade in/out container to switch between states
+  import ContainerFade from "@components/ContainerFade.svelte";
+
+  // possible user input state modes
+  import ColorInputTip from "@components/svg/ColorInputTip.svelte";
+  import StateValid from "./StateValid.svelte";
+  import StateInvalid from "./StateInvalid.svelte";
+
+  // user input prop
+  export let state;
+
+  const states = {
+    "empty": ColorInputTip,
+    "valid": StateValid,
+    null: StateInvalid
+  }
+</script>
+
+<div>
+  <svelte:component this={states[state]} />
+</div>
+
+<style>
+  div {
+    width: 100%;
+    margin: 1.5em 0 25vh 0;
+  }
+</style>
