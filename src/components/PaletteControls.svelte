2014dbf01225effe5f6bb3380a7729e838c4a451
Eric Rovell <ericrovell@gmail.com>
2020-07-21 16:06:32 +0300

    PaletteControls

---
+++ src/components/PaletteControls.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Grid from "@components/svg/Grid.svelte";
+  import Link from "@components/svg/Link.svelte";
+
+  export let row = false;
+  export let href;
+</script>
+
+<div>
+  <Grid handleClick={() => row = !row} />
+  <Link {href} />
+</div>
+
+<style>
+  div {
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: 1em;
+    grid-template-rows: 1em;
+    grid-column-gap: 0.5em;
+  }
+</style>
