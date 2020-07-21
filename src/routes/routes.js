44fed1f2210bc9d124a7f09c69ca42b524562348
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:15:02 +0300

    new palette url param

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 // https://github.com/ItalyPaleAle/svelte-spa-router
 
-import ColorSearch from "./ColorSearch.svelte";
+import SearchColor from "./Search/SearchColor.svelte";
 import Help from "@routes/Help.svelte";
 import About from "@routes/About.svelte";
 import ColorPage from "@routes/ColorPage.svelte";
@@ -8,12 +8,12 @@ import PalettePage from "@routes/PalettePage.svelte";
 
 export default {
   // exact
-  "/": ColorSearch,
+  "/": SearchColor,
   "/help": Help,
   "/about": About,
 
   // with named params
   // ex: /#/rgb/12,12,12
   "/color/:model/:values": ColorPage,
-  "/palette/:model/:values": PalettePage
+  "/palette/:colors": PalettePage
 };
