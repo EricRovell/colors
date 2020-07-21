e875b7758361ccd74ff6415e2cafb8900bdec049
Eric Rovell <ericrovell@gmail.com>
2020-07-21 16:06:20 +0300

    ColorPage

--- src/routes/ColorPage.svelte
+++ src/routes/ColorPage.svelte
@@ -1,5 +1,30 @@
 <script>
+  import Color, { Constructor } from "@components/Color.svelte";
+
   export let params = {};
+
+  function validateURL(params) {
+    const model = params.model;
+    const values = params.values.split(",").map(Number);
+
+    if (Constructor.validate({ model, values })) {
+      return {
+        model,
+        values
+      };
+    } else {
+      return null;
+    }
+  }
+
+  $: data = validateURL(params);
+  $: console.log(data);
 </script>
 
-<p>`Model is: {params.model} and values are: {JSON.stringify(params.values)}`</p>
+{#if data}
+  <div>
+    <Color {data} />
+  </div>
+{:else}
+  <p>Wrong URL</p>
+{/if}
