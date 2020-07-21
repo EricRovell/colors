00b1af90e925345e6c69c011be3a67426b5c93b3
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:20:22 +0300

    smaller padding; fixed headers case

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -2,18 +2,25 @@
   export let name;
 </script>
 
-<header id={`search-section-${name.toLowerCase()}`}>
-  {name}
-</header>
-<section>
-  <slot>
-    This section is empty...
-  </slot>
-</section>
+<article>
+  <header id={`search-section-${name.toLowerCase()}`}>
+    {name[0].toUpperCase() + name.slice(1)}
+  </header>
+  <section>
+    <slot>
+      This section is empty...
+    </slot>
+  </section>
+</article>
 
 <style>
+  article {
+    width: 100%;
+  }
+
   header {
     font-size: 1.25em;
+    margin-bottom: 0.5em;
   }
   section {
     width: 100%;
@@ -21,5 +28,11 @@
     border-radius: 5px;
     background: var(--card-bg, white);
     filter: drop-shadow(1px 1px 5px rgb(0 0 0 / 0.1));
+  }
+
+  @media screen and (max-width: 720px) {
+    section {
+      padding: 1em;
+    }
   }
 </style>
