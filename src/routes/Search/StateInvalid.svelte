28f453bc0d8d564886f74b291e57d475a53728b4
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:18:35 +0300

    using fade; moved to component dir

---
+++ src/routes/Search/StateInvalid.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { fade } from "svelte/transition";
+
+  import Question from "@components/svg/Question.svelte";
+</script>
+
+<div in:fade="{{ duration: 500 }}">
+  <Question />
+  <div>
+    Invalid or Unknown input
+  </div>
+</div>
