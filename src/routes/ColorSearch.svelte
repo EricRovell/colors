f9de928e7fba8137b26313466629b2dd23a1c805
Eric Rovell <ericrovell@gmail.com>
2020-07-21 11:19:26 +0300

    removed wrapper

--- src/routes/ColorSearch.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import ColorInput from "../ColorInput/ColorInput.svelte";
-  import FadingWrapper from "@src/layout/FadingWrapper.svelte";
-</script>
-
-<FadingWrapper>
-  <ColorInput />
-</FadingWrapper>
